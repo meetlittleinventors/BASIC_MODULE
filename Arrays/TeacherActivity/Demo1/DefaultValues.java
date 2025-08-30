6dc926ee012eefc0425c37588450d9267ebb8ed8
meetlittleinventors <meet@littleinventors.live>
2025-08-30 23:25:15 +0530

    Update and rename DefaultValues.java to Q1.java

--- Arrays/TeacherActivity/Demo1/DefaultValues.java
+++
@@ -1,8 +0,0 @@
-public class DefaultValues {
-    public static void main(String[] args) {
-        int[] arr = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
---
+++ Arrays/TeacherActivity/Demo1/Q1.java
@@ -0,0 +1,6 @@
+public class Q1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("3rd element: " + arr[2]);
+    }
+}
