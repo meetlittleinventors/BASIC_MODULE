323be5616a253881d38d024be66c853bfdc00774
meetlittleinventors <meet@littleinventors.live>
2025-08-30 15:38:30 +0530

    Delete String_manupulation/StudentActivity/Medium/SubstringExtract.java

--- String_manupulation/StudentActivity/Medium/SubstringExtract.java
+++
@@ -1,7 +0,0 @@
-public class SubstringExtract {
-    public static void main(String[] args) {
-        String word = "Programming";
-        String sub = word.substring(3, 7); // index 3 to 6
-        System.out.println("Substring: " + sub);
-    }
-}
