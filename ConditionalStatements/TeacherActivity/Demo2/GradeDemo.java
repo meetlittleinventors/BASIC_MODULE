89d6de11dab78368e9a2bb4e5f23897b73e8eab2
meetlittleinventors <meet@littleinventors.live>
2025-08-30 14:58:57 +0530

    GradeDemo.java

--- ConditionalStatements/TeacherActivity/Demo2/GradeDemo.java
+++ ConditionalStatements/TeacherActivity/Demo2/GradeDemo.java
@@ -1,6 +1,6 @@
 public class GradeDemo {
     public static void main(String[] args) {
-        int marks = 68; // try 90, 75, 50, 49, 40
+        int marks = 42;  // try changing to 92, 76, 51, 42, 30
 
         if (marks >= 90) {
             System.out.println("Grade A");
@@ -9,10 +9,9 @@ public class GradeDemo {
         } else if (marks >= 50) {
             System.out.println("Grade C");
         } else {
+            System.out.println("Fail");
             if (marks >= 40) {
-                System.out.println("Fail (Re-test eligible)");
-            } else {
-                System.out.println("Fail");
+                System.out.println("Re-test eligible");
             }
         }
     }
