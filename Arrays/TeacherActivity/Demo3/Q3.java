f0d66f66f7f2815ccdbb44396a396886747a50cf
meetlittleinventors <meet@littleinventors.live>
2025-08-30 23:25:58 +0530

    Update and rename NumberTriangle.java to Q3.java

---
+++ Arrays/TeacherActivity/Demo3/Q3.java
@@ -0,0 +1,11 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Element at row 2, column 3: " + matrix[1][2]);
+    }
+}
