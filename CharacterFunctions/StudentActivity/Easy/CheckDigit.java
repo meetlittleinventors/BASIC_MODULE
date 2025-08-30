d40db40f95b55e8081597fc7a97552042bad182b
meetlittleinventors <meet@littleinventors.live>
2025-08-30 13:51:58 +0530

    CheckDigit.java

--- CharacterFunctions/StudentActivity/Easy/CheckDigit.java
+++ CharacterFunctions/StudentActivity/Easy/CheckDigit.java
@@ -1,6 +1,6 @@
 public class CheckDigit {
     public static void main(String[] args) {
         char ch = '8';
-        System.out.println(Character.isDigit(ch)); // true
+        System.out.println("Is '" + ch + "' a digit? " + Character.isDigit(ch));
     }
 }
