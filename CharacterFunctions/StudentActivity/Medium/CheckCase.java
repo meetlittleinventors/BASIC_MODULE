e36e71d099465938bd81153b242911e0c396234b
meetlittleinventors <meet@littleinventors.live>
2025-08-30 13:52:26 +0530

    CheckCase.java

--- CharacterFunctions/StudentActivity/Medium/CheckCase.java
+++ CharacterFunctions/StudentActivity/Medium/CheckCase.java
@@ -1,10 +1,12 @@
 public class CheckCase {
     public static void main(String[] args) {
         char ch = 'm';
-        if (Character.isUpperCase(ch)) {
-            System.out.println("Uppercase");
-        } else if (Character.isLowerCase(ch)) {
-            System.out.println("Lowercase");
+        if(Character.isUpperCase(ch)) {
+            System.out.println(ch + " is Uppercase");
+        } else if(Character.isLowerCase(ch)) {
+            System.out.println(ch + " is Lowercase");
+        } else {
+            System.out.println(ch + " is neither Uppercase nor Lowercase");
         }
     }
 }
