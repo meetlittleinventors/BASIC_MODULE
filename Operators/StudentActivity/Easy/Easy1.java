a5c31cffafcdb35eb8238b727e41fa2ebb4caaa0
meetlittleinventors <meet@littleinventors.live>
2025-08-30 13:34:55 +0530

    Easy1.java

--- Operators/StudentActivity/Easy/Easy1.java
+++ Operators/StudentActivity/Easy/Easy1.java
@@ -1,10 +1,37 @@
-import java.util.Scanner;
 public class Easy1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt();
-        System.out.println("Sum = " + (a + b));
-        System.out.println("Difference = " + (a - b));
-        sc.close();
+        int a = 10, b = 3;
+
+        // Arithmetic
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a == b: " + (a == b));
+
+        // Logical
+        System.out.println("(a > 5 && b < 5): " + (a > 5 && b < 5));
+
+        // Assignment
+        int c = a;
+        c += 5;
+        System.out.println("Assignment (c += 5): " + c);
+
+        // Unary
+        int d = 5;
+        System.out.println("Pre-increment: " + (++d));
+        System.out.println("Post-decrement: " + (d--));
+
+        // Bitwise
+        System.out.println("a & b: " + (a & b));
+        System.out.println("a | b: " + (a | b));
+
+        // Ternary
+        String result = (a > b) ? "a is greater" : "b is greater";
+        System.out.println("Ternary result: " + result);
     }
 }
