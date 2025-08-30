2b7c2d219c88e67ec5f60578344ea55a76855b78
meetlittleinventors <meet@littleinventors.live>
2025-08-30 13:51:44 +0530

    CheckLetter.java

--- CharacterFunctions/StudentActivity/Easy/CheckLetter.java
+++ CharacterFunctions/StudentActivity/Easy/CheckLetter.java
@@ -1,6 +1,6 @@
 public class CheckLetter {
     public static void main(String[] args) {
         char ch = 'Z';
-        System.out.println(Character.isLetter(ch)); // true
+        System.out.println("Is '" + ch + "' a letter? " + Character.isLetter(ch));
     }
 }
