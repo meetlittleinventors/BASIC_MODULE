f66ec19afd78766f7af17da5fcacd7d06f19260c
meetlittleinventors <meet@littleinventors.live>
2025-08-30 15:36:44 +0530

    Update and rename PlaylistManager.java to ExtractWord.java

---
+++ String_manupulation/TeacherActivity/ExtractWord.java
@@ -0,0 +1,8 @@
+public class ExtractWord {
+    public static void main(String[] args) {
+        String str = "Java Programming Language";
+        String word = str.substring(5, 16);
+
+        System.out.println("Extracted Word: " + word);
+    }
+}
