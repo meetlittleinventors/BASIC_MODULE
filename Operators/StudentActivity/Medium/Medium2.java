d0abffb77c3156fe162a428c163be28837d2d435
meetlittleinventors <meet@littleinventors.live>
2025-08-30 13:35:56 +0530

    Medium2.java

--- Operators/StudentActivity/Medium/Medium2.java
+++ Operators/StudentActivity/Medium/Medium2.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+
 public class Medium2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int maths = sc.nextInt();
-        int science = sc.nextInt();
-        if(maths >= 40 && science >= 40)
+        System.out.print("Enter marks of 3 subjects: ");
+        int m1 = sc.nextInt(), m2 = sc.nextInt(), m3 = sc.nextInt();
+
+        int avg = (m1 + m2 + m3) / 3;
+
+        if(avg >= 60)
+            System.out.println("Pass with First Division");
+        else if(avg >= 40)
             System.out.println("Pass");
         else
             System.out.println("Fail");
-        sc.close();
     }
 }
