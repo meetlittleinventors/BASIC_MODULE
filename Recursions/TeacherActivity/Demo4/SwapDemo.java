12e59f7f46bda9b6769c68ba9305ebbe04c019a6
meetlittleinventors <meet@littleinventors.live>
2025-08-31 21:57:33 +0530

    Update and rename FibonacciDemo.java to SwapDemo.java

---
+++ Recursions/TeacherActivity/Demo4/SwapDemo.java
@@ -0,0 +1,15 @@
+class SwapDemo {
+    static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Inside method: a = " + a + ", b = " + b);
+    }
+
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        System.out.println("Before swap: x = " + x + ", y = " + y);
+        swap(x, y);
+        System.out.println("After swap: x = " + x + ", y = " + y);
+    }
+}
