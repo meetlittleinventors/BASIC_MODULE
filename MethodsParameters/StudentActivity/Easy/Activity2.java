893ea21edd3e80cb4a68390cf04c26dc1da4d020
meetlittleinventors <meet@littleinventors.live>
2025-08-31 21:22:10 +0530

    Update and rename Q2_Greeting.java to Activity2.java

---
+++ MethodsParameters/StudentActivity/Easy/Activity2.java
@@ -0,0 +1,11 @@
+class Activity2 {
+    static int doubleValue(int x) {
+        return x * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("double(5) = " + doubleValue(5));
+        System.out.println("double(10) = " + doubleValue(10));
+        System.out.println("double(15) = " + doubleValue(15));
+    }
+}
