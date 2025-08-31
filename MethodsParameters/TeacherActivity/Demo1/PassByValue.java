822558fc080133463b4a3e41510b2032899d6185
meetlittleinventors <meet@littleinventors.live>
2025-08-31 21:52:43 +0530

    Update and rename Demo1.java to PassByValue.java

--- MethodsParameters/TeacherActivity/Demo1/PassByValue.java
+++ MethodsParameters/TeacherActivity/Demo1/PassByValue.java
@@ -1,11 +1,11 @@
-class Demo1 {
+class PassByValue {
     static void increment(int x) {
         x = x + 10;
         System.out.println("Inside method: x = " + x);
     }
 
     public static void main(String[] args) {
-        int num = 5;
+        int num = 20;
         System.out.println("Before method: num = " + num);
         increment(num);
         System.out.println("After method: num = " + num);
