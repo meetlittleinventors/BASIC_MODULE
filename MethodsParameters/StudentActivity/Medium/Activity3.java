186c533f7282ef8bda79475c522286b9d9bb0cf6
meetlittleinventors <meet@littleinventors.live>
2025-08-31 21:46:01 +0530

    Update and rename Q3_ArraySumAverage.java to Activity3.java

---
+++ MethodsParameters/StudentActivity/Medium/Activity3.java
@@ -0,0 +1,10 @@
+class Activity3 {
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("max(5, 8) = " + max(5, 8));
+        System.out.println("max(12, 4) = " + max(12, 4));
+    }
+}
