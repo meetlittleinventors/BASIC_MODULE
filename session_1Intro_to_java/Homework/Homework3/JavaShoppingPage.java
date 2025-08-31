0279135d6c7a98fd0bfecbc5224d71e097adf739
meetlittleinventors <meet@littleinventors.live>
2025-08-31 23:36:19 +0530

    Update and rename ClassRoster.java to JavaShoppingPage.java

---
+++ session_1Intro_to_java/Homework/Homework3/JavaShoppingPage.java
@@ -0,0 +1,17 @@
+public class JavaShoppingPage {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("        JAVA SHOPPING PAGE       ");
+        System.out.println("=================================");
+        System.out.println(" Item             Price");
+        System.out.println("---------------------------------");
+        System.out.println(" 1. Laptop        $800");
+        System.out.println(" 2. Headphones    $50");
+        System.out.println(" 3. Keyboard      $25");
+        System.out.println(" 4. Mouse         $20");
+        System.out.println(" 5. Backpack      $40");
+        System.out.println("---------------------------------");
+        System.out.println("   THANK YOU FOR VISITING US!");
+        System.out.println("=================================");
+    }
+}
