0e03a70a2b64a2e13ef830b0796ff03db797bb40
meetlittleinventors <meet@littleinventors.live>
2025-08-31 23:35:57 +0530

    Update and rename RemoveLowScores.java to JavaWelcomePage.java

---
+++ session_1Intro_to_java/Homework/Homework2/JavaWelcomePage.java
@@ -0,0 +1,9 @@
+public class JavaWelcomePage {
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("*                        *");
+        System.out.println("*   WELCOME TO JAVA!!    *");
+        System.out.println("*                        *");
+        System.out.println("**************************");
+    }
+}
