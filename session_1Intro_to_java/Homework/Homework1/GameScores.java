d6f0367be33cbb179a4dc6560f238b506cd3e42a
meetlittleinventors <meet@littleinventors.live>
2025-08-31 23:35:25 +0530

    Update GameScores.java

--- session_1Intro_to_java/Homework/Homework1/GameScores.java
+++ session_1Intro_to_java/Homework/Homework1/GameScores.java
@@ -1,14 +1,14 @@
-import java.util.ArrayList;
-
-public class GameScores {
+public class AboutMe {
     public static void main(String[] args) {
-        ArrayList<Integer> scores = new ArrayList<>();
-        scores.add(85);
-        scores.add(90);
-        scores.add(75);
-        scores.add(60);
-        scores.add(95);
-
-        System.out.println("Scores: " + scores);
+        System.out.println("My name is Rahul.");
+        System.out.println("I am 16 years old.");
+        System.out.println("I study in Class 10.");
+        System.out.println("My favorite subject is Computer Science.");
+        System.out.println("I love playing cricket.");
+        System.out.println("I enjoy reading books.");
+        System.out.println("My favorite color is blue.");
+        System.out.println("I want to become a software engineer.");
+        System.out.println("I like programming in Java.");
+        System.out.println("I live in New Delhi.");
     }
 }
