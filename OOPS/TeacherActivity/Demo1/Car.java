3a8fb6c694057d5add49fe62a0a6ff62681c2b9a
meetlittleinventors <meet@littleinventors.live>
2025-09-30 20:27:18 +0530

    Update Car.java

--- OOPS/TeacherActivity/Demo1/Car.java
+++ OOPS/TeacherActivity/Demo1/Car.java
@@ -3,26 +3,26 @@ class Car {
     String model;
 
     void drive() {
-        System.out.println(color + " " + model + " is driving.");
+        System.out.println(color + " " + model + " is driving...");
     }
 
     void brake() {
-        System.out.println(color + " " + model + " has stopped.");
+        System.out.println(color + " " + model + " is braking...");
     }
 }
 
-public class Demo1 {
+public class Main {
     public static void main(String[] args) {
         Car car1 = new Car();
         car1.color = "Red";
         car1.model = "Honda";
+        car1.drive();
+        car1.brake();
 
         Car car2 = new Car();
         car2.color = "Blue";
-        car2.model = "Tesla";
-
-        car1.drive();
+        car2.model = "Toyota";
         car2.drive();
-        car1.brake();
+        car2.brake();
     }
 }
