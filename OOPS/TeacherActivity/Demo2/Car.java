57946b5ec47ab8000199f40d52e0ae2c97659909
meetlittleinventors <meet@littleinventors.live>
2025-09-30 20:27:35 +0530

    Update Car.java

--- OOPS/TeacherActivity/Demo2/Car.java
+++ OOPS/TeacherActivity/Demo2/Car.java
@@ -2,23 +2,22 @@ class Car {
     String color;
     String model;
 
-    // Constructor
     Car(String c, String m) {
         color = c;
         model = m;
     }
 
-    void showDetails() {
+    void display() {
         System.out.println("Car: " + color + " " + model);
     }
 }
 
-public class Demo2 {
+public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Red", "Honda");
-        Car car2 = new Car("Blue", "Tesla");
+        Car car2 = new Car("Blue", "Toyota");
 
-        car1.showDetails();
-        car2.showDetails();
+        car1.display();
+        car2.display();
     }
 }
