e4043d11b2c7f5cc79cee272c702abd3a51f0bd1
meetlittleinventors <meet@littleinventors.live>
2025-09-30 20:40:03 +0530

    Update and rename BankAccount.java to Student.java

---
+++ OOPS/TeacherActivity/Demo1/Student.java
@@ -0,0 +1,12 @@
+class Car {
+    void drive() {
+        System.out.println("Car is driving");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car();   // object created
+        c1.drive();           // method called
+    }
+}
