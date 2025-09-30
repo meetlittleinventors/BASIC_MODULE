af3a6b38315b185c5e89fafafbbe0d7ee6284d5a
meetlittleinventors <meet@littleinventors.live>
2025-09-30 20:50:51 +0530

    Update and rename Student.java to Calculator.java

---
+++ OOPS/TeacherActivity/Demo4/Calculator.java
@@ -0,0 +1,12 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Sum: " + calc.add(10, 20));
+    }
+}
